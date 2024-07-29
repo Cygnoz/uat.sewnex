a07c07f7e523b7198c609f8ff2f0b99bf5926991
MohdThaha <thahad2@gmail.com>
2024-07-29 16:25:21 +0530

    report

---
+++ server.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+
+const express = require('express')
+
+const cors = require('cors')
+
+const server = express()
+
+const reportRouter = require("./router/reportRouter")
+
+require('./database/connection/connection')
+
+server.use(cors())
+
+server.use(express.json())
+
+server.use(reportRouter)
+
+PORT = 5006
+
+server.get('/',(req,res)=>{
+    res.status(200).json("Bill BIZZ server started - Report ")
+})
+
+server.listen(PORT,()=>{
+    console.log(`BillBIZZ server Report started at port : ${PORT}`);
+
+})
+
