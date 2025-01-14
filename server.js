3d2d36d9068d7bf5953728dec01cc0c7c457e459
ThahaNex <mohd.thaha@cygnonex.com>
2025-01-14 10:19:58 +0530

    new

--- server.js
+++ server.js
@@ -7,7 +7,19 @@ const server = express()
 const supplierRouter = require("./router/supplierRouter")
 require('./database/connection/connection')
 
-server.use(cors())
+server.use(cors({
+    origin: "*",
+    methods: "GET, POST, PUT, DELETE, OPTIONS",
+    allowedHeaders: "Content-Type, Authorization"
+}));
+// Handle preflight requests
+server.options('*', (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.sendStatus(200);
+});
+
 server.use(express.json())
 server.use(supplierRouter)
 
