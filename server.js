32c4da001264516a048769173f51d9b46ef7800c
ThahaNex <mohd.thaha@cygnonex.com>
2025-01-14 11:59:26 +0530

    new

--- server.js
+++ server.js
@@ -3,38 +3,29 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
-const server = express();
 
+const server = express();
 const inventoryRouter = require("./router/inventoryRouter");
 require('./database/connection/connection');
 
-// Define allowed origins
-const allowedOrigins = [
-    'https://dev.billbizz.cloud', 
-    'http://localhost:5173',  
-    'http://localhost:5174'
-]; 
+server.use(express.json({ limit: '10mb' })); 
+server.use(express.urlencoded({ limit: '10mb', extended: true })); 
 
-// CORS configuration
-const corsOptions = {
-    origin: (origin, callback) => {
-        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, origin);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-    allowedHeaders: ['Content-Type', 'Authorization'], 
-    credentials: true, 
-};
+server.use(cors({
+    origin: "*",
+    methods: "GET, POST, PUT, DELETE, OPTIONS",
+    allowedHeaders: "Content-Type, Authorization"
+}));
 
-// Middleware setup
-server.use(helmet());  // Security headers
-server.use(cors());  // CORS with options
-server.use(express.json({ limit: '10mb' })); // Set limit for JSON payloads
-server.use(express.urlencoded({ limit: '10mb', extended: true })); // Set limit for URL-encoded payloads
-server.use(inventoryRouter); // Route definitions
+server.options('*', (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.sendStatus(200);
+});
+
+server.use(helmet()); 
+server.use(inventoryRouter); 
 
 const PORT = 5003
 
