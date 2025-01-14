8162617b708e254c8a20369fad38bc8e66ea4863
ThahaNex <mohd.thaha@cygnonex.com>
2025-01-14 12:02:33 +0530

    new

--- server.js
+++ server.js
@@ -1,28 +1,35 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
 
-const cors = require('cors')
-
-const server = express()
-
-server.use(express.json({ limit: '10mb' })); // Set limit to 10MB
-
-server.use(express.urlencoded({ limit: '10mb', extended: true }));
-
+const server = express();
 const staffRouter = require("./router/staffRouter")
-
 const expenseRouter = require("./router/expenseRouter")
-
 require('./database/connection/connection')
 
-server.use(cors())
+server.use(express.json({ limit: '10mb' })); 
+server.use(express.urlencoded({ limit: '10mb', extended: true }));
 
-server.use(express.json())
+server.use(cors({
+    origin: "*",
+    methods: "GET, POST, PUT, DELETE, OPTIONS",
+    allowedHeaders: "Content-Type, Authorization"
+}));
 
-server.use(staffRouter,expenseRouter)
+server.options('*', (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.sendStatus(200);
+});
 
-PORT = 5008
+
+server.use(helmet()); 
+server.use(staffRouter,expenseRouter);
+
+const PORT = 5008;
 
 server.get('/',(req,res)=>{
     res.status(200).json("Dev Bill BIZZ server started - Staff(v1)")
