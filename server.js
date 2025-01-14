12403328c41eabf9778b6de076503885406fdb65
ThahaNex <mohd.thaha@cygnonex.com>
2025-01-14 12:01:07 +0530

    new

--- server.js
+++ server.js
@@ -1,23 +1,31 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require('express')
-
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
 
 const server = express()
-
 const reportRouter = require("./router/reportRouter")
-
-
 require('./database/connection/connection')
 
-server.use(cors())
+server.use(cors({
+    origin: "*",
+    methods: "GET, POST, PUT, DELETE, OPTIONS",
+    allowedHeaders: "Content-Type, Authorization"
+}));
 
+server.options('*', (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.sendStatus(200);
+});
+
+server.use(helmet()); 
 server.use(express.json())
-
 server.use(reportRouter)
 
-PORT = 5006
+const PORT = 5006;
 
 server.get('/',(req,res)=>{
     res.status(200).json("Dev Bill BIZZ server started - Report ")
@@ -27,4 +35,3 @@ server.listen(PORT,()=>{
     console.log(`BillBIZZ server Report started at port : ${PORT}`);
 
 })
-
