9a4f006fd78bf3944b985e913bee605dc1d9dc77
MohdThaha <165124426+MohdThaha@users.noreply.github.com>
2025-02-13 08:45:54 +0530

    mew

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ server.use(supplierRouter)
 const PORT = 5009
 
 server.get('/',(req,res)=>{
-    res.status(200).json("Dev Bill BIZZ server started - Supplier")
+    res.status(200).json("Dev Bill BIZZ server started - Supplier v1.6")
 })
 
 server.listen(PORT,()=>{
