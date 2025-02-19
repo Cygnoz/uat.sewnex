7e8f57b143e3a74a495d8b62f7e4254ae8ff1126
Anju <anjup3994@gmail.com>
2025-02-19 09:25:38 +0530

    new

--- router/expenseRouter.js
+++ router/expenseRouter.js
@@ -11,21 +11,21 @@ const { nexVerifyToken } = require('../controller/nexMiddleware');
 
 
 // expense
-router.post('/add-expense', verifyToken, expenseController.addExpense);
-router.get('/get-all-expense', verifyToken, expenseController.getAllExpense);
-router.get('/get-one-expense/:expenseId', verifyToken, expenseController.getOneExpense);
-router.get('/get-last-expense-prefix', verifyToken, expenseController.getLastExpensePrefix);
-router.put('/update-expense/:expenseId', verifyToken, updateExpense.updateExpense);
-router.delete('/delete-expense/:expenseId', verifyToken, updateExpense.deleteExpense);
-router.get('/expense-journal/:expenseId',verifyToken,expenseController.expenseJournal);
+router.post('/add-expense', verifyToken, checkPermission("Created a New Expense"), expenseController.addExpense);
+router.get('/get-all-expense', verifyToken, checkPermission("Viewed Expense Details"), expenseController.getAllExpense);
+router.get('/get-one-expense/:expenseId', verifyToken, checkPermission("Viewed Expense Details"), expenseController.getOneExpense);
+router.get('/get-last-expense-prefix', verifyToken, checkPermission("Created a New Expense"), expenseController.getLastExpensePrefix);
+router.put('/update-expense/:expenseId', verifyToken, checkPermission("Edited Expense Information"), updateExpense.updateExpense);
+router.delete('/delete-expense/:expenseId', verifyToken, checkPermission("Deleted Expense Information"), updateExpense.deleteExpense);
+router.get('/expense-journal/:expenseId', verifyToken, checkPermission("Viewed Expense Details"), expenseController.expenseJournal);
 
 
 // expenseCategory
-router.post('/add-category', verifyToken, expenseController.addCategory);
-router.get('/get-all-category', verifyToken, expenseController.getAllCategory);
-router.get('/get-one-category/:categoryId', verifyToken, expenseController.getACategory);
-router.put('/update-category/:categoryId', verifyToken, expenseController.updateCategory);
-router.delete('/delete-category/:categoryId', verifyToken, expenseController.deleteCategory);
+router.post('/add-category', verifyToken, checkPermission("Created a New Expense Category"), expenseController.addCategory);
+router.get('/get-all-category', verifyToken, checkPermission("Viewed Expense Category Details"), expenseController.getAllCategory);
+router.get('/get-one-category/:categoryId', verifyToken, checkPermission("Viewed Expense Category Details"), expenseController.getACategory);
+router.put('/update-category/:categoryId', verifyToken, checkPermission("Edited Expense Category Information"), expenseController.updateCategory);
+router.delete('/delete-category/:categoryId', verifyToken, checkPermission("Deleted Expense Category Information"), expenseController.deleteCategory);
 
 
 
@@ -42,19 +42,19 @@ router.delete('/delete-category/:categoryId', verifyToken, expenseController.del
 
 
 //nexPortal
-router.post('/add-expense-nexportal', nexVerifyToken, expenseController.addExpense);
-router.get('/get-all-expense-nexportal', nexVerifyToken, expenseController.getAllExpense);
-router.get('/get-one-expense-nexportal/:expenseId', nexVerifyToken, expenseController.getOneExpense);
-router.get('/get-last-expense-prefix-nexportal', nexVerifyToken, expenseController.getLastExpensePrefix);
-router.put('/update-expense-nexportal/:expenseId', nexVerifyToken, updateExpense.updateExpense);
-router.delete('/delete-expense-nexportal/:expenseId', nexVerifyToken, updateExpense.deleteExpense);
-router.get('/expense-journal-nexportal/:expenseId',nexVerifyToken,expenseController.expenseJournal);
+router.post('/add-expense-nexportal', nexVerifyToken, checkPermission('Created a New Expense'), expenseController.addExpense);
+router.get('/get-all-expense-nexportal', nexVerifyToken, checkPermission('Viewed Expense Details'), expenseController.getAllExpense);
+router.get('/get-one-expense-nexportal/:expenseId', nexVerifyToken, checkPermission('Viewed Expense Details'), expenseController.getOneExpense);
+router.get('/get-last-expense-prefix-nexportal', nexVerifyToken, checkPermission('Created a New Expense'), expenseController.getLastExpensePrefix);
+router.put('/update-expense-nexportal/:expenseId', nexVerifyToken, checkPermission('Edited Expense Information'), updateExpense.updateExpense);
+router.delete('/delete-expense-nexportal/:expenseId', nexVerifyToken, checkPermission('Deleted Expense Information'), updateExpense.deleteExpense);
+router.get('/expense-journal-nexportal/:expenseId', nexVerifyToken, checkPermission('Viewed Expense Details'), expenseController.expenseJournal);
 
-router.post('/add-category-nexportal', nexVerifyToken, expenseController.addCategory);
-router.get('/get-all-category-nexportal', nexVerifyToken, expenseController.getAllCategory);
-router.get('/get-one-category-nexportal/:categoryId', nexVerifyToken, expenseController.getACategory);
-router.put('/update-category-nexportal/:categoryId', nexVerifyToken, expenseController.updateCategory);
-router.delete('/delete-category-nexportal/:categoryId', nexVerifyToken, expenseController.deleteCategory);
+router.post('/add-category-nexportal', nexVerifyToken, checkPermission('Created a New Expense Category'), expenseController.addCategory);
+router.get('/get-all-category-nexportal', nexVerifyToken, checkPermission('Viewed Expense Category Details'), expenseController.getAllCategory);
+router.get('/get-one-category-nexportal/:categoryId', nexVerifyToken, checkPermission('Viewed Expense Category Details'), expenseController.getACategory);
+router.put('/update-category-nexportal/:categoryId', nexVerifyToken, checkPermission('Edited Expense Category Information'), expenseController.updateCategory);
+router.delete('/delete-category-nexportal/:categoryId', nexVerifyToken, checkPermission('Deleted Expense Category Information'), expenseController.deleteCategory);
 
 
 module.exports = router
