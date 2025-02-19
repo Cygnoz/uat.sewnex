65c388f618b9377fde5fb210c86d22a3c21d3e73
Anju <anjup3994@gmail.com>
2025-02-19 10:05:04 +0530

    new

--- router/reportRouter.js
+++ router/reportRouter.js
@@ -1,15 +1,17 @@
 const express = require("express")
 const dayBook = require("../controller/dayBook");
 const trialBalance = require("../controller/trialBalance");
-const { verifyToken } = require('../controller/middleware');
 const reports = require("../controller/reports");
+
+const checkPermission = require('../controller/permission');
+const { verifyToken } = require('../controller/middleware');
 
 const router = new express.Router()
 
-router.get("/dayBook/:startDate/:endDate", verifyToken, dayBook.getDayBook);
-router.get("/trialBalance/:startDate/:endDate", verifyToken, trialBalance.getTrialBalance);
-router.get("/tradingAccount/:startDate/:endDate", verifyToken, reports.calculateTradingAccount);
-router.get("/profitAndLoss/:startDate/:endDate", verifyToken, reports.calculateProfitAndLoss);
-router.get("/balanceSheet/:startDate/:endDate", verifyToken, reports.calculateBalanceSheet);
+router.get("/dayBook/:startDate/:endDate", verifyToken, checkPermission("Viewed Reports"), dayBook.getDayBook);
+router.get("/trialBalance/:startDate/:endDate", verifyToken, checkPermission("Viewed Reports"), trialBalance.getTrialBalance);
+router.get("/tradingAccount/:startDate/:endDate", verifyToken, checkPermission("Viewed Reports"), reports.calculateTradingAccount);
+router.get("/profitAndLoss/:startDate/:endDate", verifyToken, checkPermission("Viewed Reports"), reports.calculateProfitAndLoss);
+router.get("/balanceSheet/:startDate/:endDate", verifyToken, checkPermission("Viewed Reports"), reports.calculateBalanceSheet);
 
 module.exports = router
