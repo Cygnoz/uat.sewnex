b1685e4c4654cf4ff3c7b87ca720a98450fd3e3d
MohdThaha <165124426+MohdThaha@users.noreply.github.com>
2025-03-10 11:07:30 +0530

    new

--- router/staffRouter.js
+++ router/staffRouter.js
@@ -10,7 +10,9 @@ const { verifyToken } = require('../controller/middleware');
 // Staff
 router.post('/add-staff', verifyToken, staffController.addStaff);
 router.put('/edit-staff/:staffId', verifyToken, staffController.editStaff);
-
+router.get('/get-staff', verifyToken, staffController.getAllStaff);
+router.get('/get-staff/:staffId', verifyToken, staffController.getStaffById);
+router.delete('/delete-staff/:staffId', verifyToken, staffController.deleteStaff);
 
 
 module.exports = router
