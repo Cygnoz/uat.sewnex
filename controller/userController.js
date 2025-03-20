25f6c1f9f8d5e2142962ef13bcd7f2c0e754cf54
MohdThaha <165124426+MohdThaha@users.noreply.github.com>
2025-03-20 11:16:38 +0530

    new

--- controller/userController.js
+++ controller/userController.js
@@ -222,3 +222,112 @@ BillBizz`,
     return false;
   }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+// Login 
+exports.loginOTP = async (req, res) => {
+  try {
+    // Get all data
+    const { email, password } = req.body;    
+
+    // Validate input
+    if (!email || !password) {
+      return res.status(400).json({ success: false, message: 'Please provide both email and password' });
+    }
+
+    // Find the user
+    const user = await User.findOne({ userEmail:email });
+
+    // Check if user exists
+    if (!user) {
+      return res.status(401).json({ success: false, message: 'User not found!' });
+    }
+
+    const organization = await Organization.findOne({ organizationId: user.organizationId });
+
+    // Check if organization exists
+    if (!organization) {
+      return res.status(401).json({ success: false, message: 'Organization not found!' });
+    }
+    // Check if organization is active
+    if (!organization.isActive) {
+      return res.status(401).json({ success: false, message: 'Organization is not active!' });
+    }
+    // Check if user is active
+    if (!user.isActive) {
+      return res.status(401).json({ success: false, message: 'User is not active!' });
+    }
+
+    // Match the password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ success: false, message: 'Invalid Password!' });
+    }
+
+
+
+    // Capture IP address and User-Agent during verification
+    const requestIP = req.ip || req.connection.remoteAddress; // Get IP address
+    const requestUserAgent = req.headers['user-agent']; // Get User-Agent (browser/device info)
+
+    console.log("Request IP :",requestIP);
+    console.log("Request User Agent :",requestUserAgent);
+
+    const isMobile = /mobile|tablet|android|iphone|ipad|ipod/i.test(requestUserAgent);
+    const deviceType = isMobile ? "Mobile" : "Desktop";
+
+    console.log("Device Type:", deviceType);  
+    
+    const role = await Role.findOne({ organizationId: user.organizationId ,roleName: user.role }).lean();      
+
+    
+    // Create JWT token with user ID and organizationId
+    const token = jwt.sign(
+      {
+        id: user._id,
+        organizationId: user.organizationId,
+        organizationName: organization.organizationName,
+        userName: user.userName,
+        ip: requestIP,  // Bind IP address
+        userAgent: requestUserAgent,  // Bind User-Agent (browser/device)
+        iat: Math.floor(Date.now() / 1000), // issued at time
+        nbf: Math.floor(Date.now() / 1000), // token valid from now 
+      },
+      process.env.JWT_SECRET, // JWT secret from environment variables
+      // { expiresIn: '12h' }
+    );
+
+    // Remove sensitive data from response
+    user.password = undefined;
+
+    // Send response with user data (excluding organizationId)
+    res.status(200).json({
+      success: true,
+      token: `Bearer ${token}`, // Prepend "Bearer " to the token
+      user: {
+        id: user._id,
+        email: user.userEmail,
+        userName: user.userName,
+        role: user.role,
+        organizationName: organization.organizationName,
+        permission: role?.permissions,
+        deviceType
+      },
+    });
+
+    
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: 'Internal Server Error' });
+  }
+};
